de80a7f4384af2347fa0a0b6209887ad50f27b28
Zverina1337 <80909276+Zverina1337@users.noreply.github.com>
2022-11-24 13:57:34 +0700

    Begin

---
+++ ts-vue/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <router-link to="/" class="link">Home</router-link>
+    <router-link to="/auth" class="link">Войти в аккаунт</router-link>
+    <router-link to="/orders" class="link">Заказы</router-link>
+    <router-link to="/basket" class="link">Корзина</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style scoped>
+
+</style>
